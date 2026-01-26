35f19a36a174b5a0a894a9a3e3f82ebbd1d6a76a
Shafqa Fatma <fatmashafqa4@gmail.com>
2026-01-26 09:50:36 +0530

    homepage

--- travel-frontend/src/components/homepage.jsx
+++ travel-frontend/src/components/homepage.jsx
@@ -3,7 +3,32 @@ import react from "react"
 export const HomePage = ()=>{
     return (
        <>
-       
+       <div>
+        <div>
+            {data.map((d)=>(
+                <div>
+                    <div>
+                    <img src="" alt="" />
+                    </div>
+
+                    <div>
+                        heading
+                    </div>
+                    <p></p>
+                    <button></button>
+                </div>
+
+                
+            ))}
+        </div>
+       </div>
        </>
     )
 }
+
+
+const data = [
+    {
+        
+    }
+]
--- travel-frontend/src/components/navbar.jsx
+++ travel-frontend/src/components/navbar.jsx
@@ -6,8 +6,9 @@ export const Navbar = () => {
         <>
 
             <nav className="fixed top-6 left-0 right-0 z-50 ">
-                <div className="mx-auto bg-white/80 rounded-4xl p-4 w-3/4 max-w-5xl">
-                    <div className="flex justify-between items-center border rounded-4xl bg-primary-green/80 p-4  mx-auto  backdrop-blur-md shadow-lg">
+                
+                <div className="mx-auto bg-white/80 rounded-4xl p-4 w-3/4 max-w-5xl z-50 relative">
+                    <div className="flex justify-between items-center border rounded-4xl bg-primary-green/80 p-4  mx-auto  backdrop-blur-md shadow-lg ">
                         <div>
                             <h1 className="text-4xl underline">GooMo</h1>
                         </div>
@@ -56,6 +57,10 @@ export const Navbar = () => {
                     </div>
                 </div>
             </nav>
+
+            <div className="absolute w-full h-screen overflow-hidden">
+            <img src="https://images.contentstack.io/v3/assets/bltcedd8dbd5891265b/bltcb5c5add9f186f0d/664cbc617f154494ea151f48/4832845-AdobeStock_216195684.jpeg?q=70&width=640&auto=webp" alt="" className="z-0 relative w-full h-full object-cover " />
+            </div>
         </>
     )
 }
